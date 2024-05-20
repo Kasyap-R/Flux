c4e25cc6b4139503cbc14de26ba747663ab8a367
Kasyap-R <kasyap.rachuri2@gmail.com>
2024-05-19 23:33:14 -0700

    Adding new markdown parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
-use core::{fmt, num};
-use std::collections::HashMap;
+use core::fmt;
 use std::fs::File;
 use std::hash::Hash;
 use std::io::{self, Read, Write};
@@ -41,240 +40,203 @@ impl FileType {
 }
 
 #[derive(Clone, Debug, Hash, Eq, PartialEq)]
-enum MarkdownToken {
-    Bold,
-    Italic,
-    BoldItalic,
+enum MarkdownState {
+    BOLD,
+    ITALIC,
     InlineCode,
     CodeBlock,
-    Header1,
-    Header2,
-    Header3,
-    UnorderedList,
-    Text(String),
+    LINK,
+    TEXT,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::io;
-
-    #[test]
-    fn validate_html() -> io::Result<()> {
-        let mut real_file = File::open("test_files/baby.html")?;
-        let mut real_contents = String::new();
-        real_file.read_to_string(&mut real_contents)?;
-        real_contents = real_contents.trim().to_string();
-
-        let mut test_file = File::open("test_files/test.html")?;
-        let mut test_contents = String::new();
-        test_file.read_to_string(&mut test_contents)?;
-        test_contents = test_contents.trim().to_string();
-        assert_eq!(real_contents, test_contents);
-        Ok(())
-    }
+struct MDParserState {
+    text: String,
+    html: String,
+    length: usize,
+    index: usize,
+    state: MarkdownState,
+    list_level: usize,
 }
 
-fn md_tags_to_tokens() -> HashMap<String, MarkdownToken> {
-    use MarkdownToken::*;
-    let mut map = HashMap::new();
-    map.insert("#".to_string(), Header1);
-    map.insert("##".to_string(), Header2);
-    map.insert("###".to_string(), Header3);
-    map.insert("***".to_string(), BoldItalic);
-    map.insert("**".to_string(), Bold);
-    map.insert("*".to_string(), Italic);
-    map.insert("`".to_string(), InlineCode);
-    map.insert("```".to_string(), CodeBlock);
-    map.insert("-".to_string(), UnorderedList);
-    map
-}
-
-fn md_tokens_to_html_openers() -> HashMap<MarkdownToken, String> {
-    use MarkdownToken::*;
-    let mut map = HashMap::new();
-    map.insert(Header1, "<h1>".to_string());
-    map.insert(Header2, "<h2>".to_string());
-    map.insert(Header3, "<h3>".to_string());
-    map.insert(BoldItalic, "<bold><em>".to_string());
-    map.insert(Bold, "<bold>".to_string());
-    map.insert(Italic, "<em>".to_string());
-    map.insert(InlineCode, "<code>".to_string());
-    map.insert(CodeBlock, "<code>".to_string());
-    map.insert(UnorderedList, "<ul>".to_string());
-    map
-}
-fn md_tokens_to_html_closers() -> HashMap<MarkdownToken, String> {
-    use MarkdownToken::*;
-    let mut map = HashMap::new();
-    map.insert(Header1, "</h1>".to_string());
-    map.insert(Header2, "</h2>".to_string());
-    map.insert(Header3, "</h3>".to_string());
-    map.insert(BoldItalic, "</bold></em>".to_string());
-    map.insert(Bold, "</bold>".to_string());
-    map.insert(Italic, "</em>".to_string());
-    map.insert(InlineCode, "</code>".to_string());
-    map.insert(CodeBlock, "</code>".to_string());
-    map.insert(UnorderedList, "</ul>".to_string());
-    map
-}
-
-fn md_openers_to_closers() -> HashMap<String, String> {
-    let mut map = HashMap::new();
-    map.insert("#".to_string(), "\n".to_string());
-    map.insert("##".to_string(), "\n".to_string());
-    map.insert("###".to_string(), "\n".to_string());
-    map.insert("***".to_string(), "***".to_string());
-    map.insert("**".to_string(), "**".to_string());
-    map.insert("*".to_string(), "*".to_string());
-    map.insert("```".to_string(), "```".to_string());
-    map.insert("`".to_string(), "`".to_string());
-    map.insert("-".to_string(), "\n".to_string());
-
-    map
-}
-
-fn validate_md(md_contents: &str) -> bool {
-    // for each char in md
-    //      if opener:
-    //          write openers equivalent to file
-    //          if opener has a closer:
-    //              push corresponding coser to stack
-    //      else if closer:
-    //          if closer is top element of stack
-    //              pop
-    //              write closers equivalent to file
-    //          else:
-    //              return an error
-    //      else:
-    //          write char to file as is
-
-    false
-}
-
-// Goal: Create a Markdown Tokenizer given a string of valid markdown using a state machine
-// What is a markdown token? A markdown tag such as "*" or "###" is a markdownt token
-// a character is also a valid token, given it isn't a tog or part of a tag
-// the state you're in changes depending on the tag most recently processed
-// For example, if you processed "**", you are in a bold state until you find a corresponding "**"
-// while in bold state you can still add raw characters, which is what you do until you find an end
-// tag
-// Not all markdown tags have a closing tag, but all HTML tags do, so for some states, a newline
-// will denote the end of that state
-// No nesting of tags so when we find a tag, we continuously add chars until we find its end
-
-fn tokenize_md(md_contents: &str) -> Vec<MarkdownToken> {
-    use MarkdownToken::*;
-    println!("{}", md_contents);
-    let len = md_contents.len();
-    let tags_to_tokens = md_tags_to_tokens();
-    let openers_to_closers = md_openers_to_closers();
-
-    let mut tokens: Vec<MarkdownToken> = Vec::new();
-    let mut i = 0;
-
-    let valid_tags: Vec<String> = vec![
-        "***".to_string(),
-        "**".to_string(),
-        "*".to_string(),
-        "###".to_string(),
-        "##".to_string(),
-        "#".to_string(),
-        "```".to_string(),
-        "`".to_string(),
-        "-".to_string(),
-    ];
-    while i < len {
-        let mut is_tag = false;
-        for opener_tag in &valid_tags {
-            if !md_contents[i..].starts_with(opener_tag) {
-                continue;
-            }
-            is_tag = true;
-            let token = match tags_to_tokens.get(opener_tag) {
-                Some(t) => t.clone(),
-                None => panic!(),
-            };
-            let closer_tag: String = match openers_to_closers.get(opener_tag) {
-                Some(tag) => tag.clone(),
-                None => panic!(),
-            };
-            let closer_len = closer_tag.len();
-            let token_len = opener_tag.len();
-            tokens.push(token.clone());
-            if let Some(end) = md_contents[i + token_len..].find(&closer_tag) {
-                let adjusted_end = i + token_len + end;
-                tokens.push(Text(md_contents[i + token_len..adjusted_end].to_string()));
-                tokens.push(token);
-                i = adjusted_end + closer_len;
-            } else {
-                panic!();
-            }
-            break;
-        }
-        if !is_tag {
-            tokens.push(Text(md_contents[i..i + 1].to_string()));
-            i += 1;
+impl MDParserState {
+    fn md_init_parser(mut md_file: File) -> Self {
+        let mut text = String::new();
+        let html = String::new();
+        md_file.read_to_string(&mut text).expect("Read Error");
+        text = text
+            .lines()
+            .map(str::trim)
+            .collect::<Vec<&str>>()
+            .join("\n");
+        let length = text.len();
+        MDParserState {
+            text,
+            html,
+            length,
+            index: 0,
+            state: MarkdownState::TEXT,
+            list_level: 0,
         }
     }
 
-    tokens
-}
+    fn handle_header(&mut self) {
+        let mut level = 0;
+        let char = self.get_ith_char(self.index).unwrap();
+        while self.index < self.length && char == '#' {
+            level += 1;
+            self.index += 1;
+        }
+        if char == ' ' {
+            self.index += 1;
+        }
+        let header_text = self.parse_inline();
+        self.html
+            .push_str(&format!("<h{}>{}</h{}>\n", level, header_text, level));
+    }
 
-fn token_to_html(token: &MarkdownToken, map: &HashMap<MarkdownToken, String>) -> String {
-    match token {
-        MarkdownToken::Text(s) => s.replace('\n', "<br>\n"),
-        _ => map
-            .get(token)
-            .cloned()
-            .unwrap_or_else(|| panic!("No matching HTML opener for token")),
+    fn handle_bold_italic(&mut self) {
+        if &self.text[self.index..=self.index + 1] == "**" {
+            self.state = MarkdownState::BOLD;
+            self.index += 2;
+            let bold_text = self.parse_inline();
+            self.html
+                .push_str(&format!("<strong>{}</strong>\n", bold_text));
+        } else if self.get_ith_char(self.index).unwrap() == '*' {
+            self.state = MarkdownState::ITALIC;
+            self.index += 1;
+            let italic_text = self.parse_inline();
+            self.html.push_str(&format!("<em>{}</em>\n", italic_text))
+        }
+        self.state = MarkdownState::TEXT;
+    }
+
+    fn handle_link(&mut self) {
+        self.state = MarkdownState::LINK;
+        self.index += 1;
+        let mut link_text = "".to_string();
+        while self.index < self.length && self.get_ith_char(self.index).unwrap() != ']' {
+            link_text.push(self.get_ith_char(self.index).unwrap());
+            self.index += 1;
+        }
+        self.index += 2; // Skip ']('
+        let mut link_url = "".to_string();
+        while self.index < self.length && self.get_ith_char(self.index).unwrap() != ')' {
+            link_url.push(self.get_ith_char(self.index).unwrap());
+            self.index += 1;
+        }
+        self.html
+            .push_str(&format!("<a href={}>{}</a>\n", link_url, link_text));
+        self.state = MarkdownState::TEXT;
+    }
+
+    fn handle_code(&mut self) {
+        if &self.text[self.index..=self.index + 2] == "```" {
+            self.state = MarkdownState::CodeBlock;
+            self.index += 3;
+            let mut code_block = "".to_string();
+            while self.index < self.length && &self.text[self.index..=self.index + 2] != "```" {
+                code_block.push(self.get_ith_char(self.index).unwrap());
+                self.index += 1;
+            }
+            self.index += 3;
+            self.html
+                .push_str(&format!("<pre><code>{}</code></pre>\n", code_block));
+        } else if self.get_ith_char(self.index).unwrap() == '`' {
+            self.state = MarkdownState::InlineCode;
+            self.index += 1;
+            let mut code_text = "".to_string();
+            while self.index < self.length && self.get_ith_char(self.index).unwrap() != '`' {
+                code_text.push(self.get_ith_char(self.index).unwrap());
+                self.index += 1;
+            }
+            self.index += 1;
+            self.html.push_str(&format!("<code>{}</code>\n", code_text));
+        }
+        self.state = MarkdownState::TEXT;
+    }
+
+    fn handle_ordered_list(&mut self) {
+        if self.list_level == 0 {
+            self.html.push_str("<ol>\n");
+        }
+        self.list_level += 1;
+        self.index += 1;
+        let ordered_list_item = self.parse_inline();
+        self.html
+            .push_str(&format!("    <li>{}</li>\n", ordered_list_item.trim()));
+        self.list_level -= 1;
+        if self.list_level == 0 {
+            self.html.push_str("</ol>\n");
+        }
+    }
+
+    fn handle_list(&mut self) {
+        if self.list_level == 0 {
+            self.html.push_str("<ul>\n");
+        }
+        self.list_level += 1;
+        self.index += 1;
+        let list_item = self.parse_inline();
+        self.html
+            .push_str(&format!("    <li>{}</li>\n", list_item.trim()));
+        self.list_level -= 1;
+        if self.list_level == 0 {
+            self.html.push_str("</ul>\n");
+        }
+    }
+
+    fn parse_inline(&mut self) -> String {
+        let mut inline_html = "".to_string();
+        let char = self.get_ith_char(self.index).unwrap();
+        while self.index < self.length && "\n#".contains(char) {
+            match char {
+                '*' => {
+                    self.handle_bold_italic();
+                }
+                '[' => {
+                    self.handle_link();
+                }
+            }
+        }
+        return "".to_string();
+    }
+
+    fn get_ith_char(&self, index: usize) -> Option<char> {
+        self.text.chars().nth(index)
     }
 }
 
 fn md_to_html(md_path: &str) -> Result<String, &'static str> {
-    use MarkdownToken::*;
-    let mut converted_string = String::new();
-    let mut md_file = File::open(md_path).expect("IO Error");
-    let mut md_file_contents = String::new();
-    md_file
-        .read_to_string(&mut md_file_contents)
-        .expect("Read Error");
-    md_file_contents = md_file_contents
-        .lines()
-        .map(str::trim)
-        .collect::<Vec<&str>>()
-        .join("\n");
-    // This last new line is needed for tokenization
-    md_file_contents.push('\n');
-    let tokens = tokenize_md(&md_file_contents);
-    let openers = md_tokens_to_html_openers();
-    let closers = md_tokens_to_html_closers();
-    println!("{:#?}", tokens);
-    let mut in_tag = false;
-    // Will have to use a stack to keep track of most recent tag if I allow nested tag in future
-    for token in tokens {
-        let mut html: String;
-        if !in_tag {
-            match token {
-                Text(_) => (),
-                _ => in_tag = true,
-            }
-            html = token_to_html(&token, &openers);
-        } else {
-            html = token_to_html(&token, &closers);
-            match token {
-                Text(_) => (),
-                _ => {
-                    in_tag = false;
-                    html.push('\n');
+    use MDParserState;
+    use MarkdownState::*;
+    let md_file = File::open(md_path).expect("IO Error");
+    let mut parser = MDParserState::md_init_parser(md_file);
+    println!("{}", &parser.text);
+
+    while parser.index < parser.length {
+        let i = parser.index;
+        let char: char = parser.get_ith_char(i).unwrap();
+        if parser.state == TEXT {
+            match char {
+                '#' => parser.handle_header(),
+                '*' => parser.handle_bold_italic(),
+                '[' => parser.handle_link(),
+                '`' => parser.handle_code(),
+                '\n' => parser.html.push_str("<br>\n"),
+                '-' if parser.get_ith_char(i + 1).unwrap() == '.' => {
+                    parser.handle_list();
                 }
+                _ if char.is_digit(10) && parser.get_ith_char(i + 1).unwrap() == '.' => {
+                    parser.handle_ordered_list();
+                }
+
+                _ => parser.html.push(char),
             }
         }
-        converted_string.push_str(&html);
+        parser.index += 1;
     }
-    println!("{}", converted_string);
 
-    Ok(converted_string)
+    Ok(parser.html)
 }
 
 fn main() -> io::Result<()> {
@@ -293,4 +255,25 @@ fn main() -> io::Result<()> {
     output_file.write_all(converted_data.as_bytes())?;
     output_file.flush()?; */
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io;
+
+    #[test]
+    fn validate_html() -> io::Result<()> {
+        let mut real_file = File::open("test_files/baby.html")?;
+        let mut real_contents = String::new();
+        real_file.read_to_string(&mut real_contents)?;
+        real_contents = real_contents.to_string();
+
+        let mut test_file = File::open("test_files/test.html")?;
+        let mut test_contents = String::new();
+        test_file.read_to_string(&mut test_contents)?;
+        test_contents = test_contents.trim().to_string();
+        assert_eq!(real_contents, test_contents);
+        Ok(())
+    }
 }
